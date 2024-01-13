0c0cb83c6a6f0721b144558fdb2f264ff78eb4e8
afsoongh <afsoon.gharib@gmail.com>
2024-01-12 16:50:51 -0800

    finishing up the wall map and training mode

--- src/omniroute_operation/src/experiment_controller/interface.py
+++ src/omniroute_operation/src/experiment_controller/interface.py
@@ -134,6 +134,8 @@ class Interface(Plugin):
         self.current_trial_index = 0
         
         self.id = 0
+        self.training_mode = None
+
 
         r = rospy.Rate(100)
         while not rospy.is_shutdown():
@@ -276,9 +278,11 @@ class Interface(Plugin):
 
     def _handle_trainingModeBtnGroup_clicked(self):
         if self._widget.trainingModeBtnGroup.checkedId() == 1:
-            self.setForcedChoiceMode()
+            self.training_mode = "user_defined_forced_choice"
         elif self._widget.trainingModeBtnGroup.checkedId() == 2:
-            self.setChoiceMode()
+            self.training_mode = "user_defined_choice"
+        elif self._widget.trainingModeBtnGroup.checkedId() == 3:
+            self.setAutomaticMode()
 
 
     #In the following functions, we define the starting maze configuration for each chamber. 
@@ -304,13 +308,11 @@ class Interface(Plugin):
         self.left_chamber = [5]
         self.right_chamber = [3]
         self.start_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7]]
-        self.start_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7]]
         self.project_left_cue_wall = [4]
         self.project_right_cue_wall = [0]
         self.left_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3 ,5, 6, 7], [1, 3, 4, 5, 7]]
-        self.left_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7]]
         self.right_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7]]
-        self.right_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7]]
+        self.both_doors_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [1, 2, 3, 5, 6, 7], [1, 3, 4, 5, 7]]
         self.wallStates = WallState()
         self.wallStates.chamber = self.chambers_list
         self.wallStates.wall = self.walls_list
@@ -323,13 +325,11 @@ class Interface(Plugin):
         self.left_chamber = [1]
         self.right_chamber = [7]
         self.start_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-        self.start_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
         self.project_left_cue_wall = [2]
         self.project_right_cue_wall = [6]
         self.left_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-        self.left_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
         self.right_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 7], [1, 3, 5, 6, 7]]
-        self.right_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
+        self.both_doors_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [1, 2, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.wallStates = WallState()
         self.wallStates.chamber = self.chambers_list
         self.wallStates.wall = self.walls_list
@@ -342,13 +342,11 @@ class Interface(Plugin):
         self.left_chamber = [7]
         self.right_chamber = [1]
         self.start_door_open = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        self.start_door_close = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.project_left_cue_wall = [6]
         self.project_right_cue_wall = [2]
         self.left_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        self.left_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.right_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        self.right_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
+        self.both_doors_open = [[1, 2, 3, 5, 7], [0, 1, 3, 4, 5, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.wallStates = WallState()
         self.wallStates.chamber = self.chambers_list
         self.wallStates.wall = self.walls_list
@@ -361,58 +359,17 @@ class Interface(Plugin):
         self.left_chamber = [3]
         self.right_chamber = [5]
         self.start_door_open = [[0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        self.start_door_close = [[0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.project_left_cue_wall = [1]
         self.project_right_cue_wall = [2]
         self.left_goal_door_open = [[0, 1, 3, 5, 7], [1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        self.left_goal_door_close = [[0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.right_goal_door_open = [[0, 1, 3, 5, 7], [0, 1, 2, 3 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        self.right_goal_door_close = [[0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
+        self.both_doors_open = [[0, 1, 3, 5, 7], [1, 2, 3, 5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
         self.wallStates = WallState()
         self.wallStates.chamber = self.chambers_list
         self.wallStates.wall = self.walls_list
         self.wallStates.state = True
         self.door_pub.publish(self.wallStates)
 
-
-    def setForcedChoiceMode(self):
-        if self.id == 1:
-            self.left_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-            self.left_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-        elif self.id == 3:
-            self.left_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-            self.left_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 5, 6, 7]]
-        elif self.id == 5:
-            self.left_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-            self.left_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_open = [[1, 2, 3, 5, 7], [0, 1, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_close = [[1, 2, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-        elif self.id == 7:
-            self.left_goal_door_open = [[0, 1, 3, 5, 7], [1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-            self.left_goal_door_close = [[0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_open = [[0, 1, 3, 5, 7], [0, 1, 2, 3 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-            self.right_goal_door_close = [[0, 1, 3, 5, 7], [0, 1, 2, 3, 4 ,5, 6, 7], [1, 3, 4, 5, 7], [1, 3, 5, 6, 7]]
-
-    def setChoiceMode(self):
-        if self.id == 1:
-            self.door_map_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            self.door_map_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            
-        elif self.id == 3:
-            self.door_map_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            self.door_map_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            
-        elif self.id == 5:
-            self.door_map_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            self.door_map_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            
-        elif self.id == 7:
-            self.door_map_open = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
-            self.door_map_close = [[1, 2, 3, 5, 7], [0, 1, 3, 5, 7]]
 
     def setAutomaticMode(self):
         self.currentTrialNumber = self.current_trial_index 
@@ -423,37 +380,8 @@ class Interface(Plugin):
             self.currentTrial = None
 
         if self.currentTrial is not None:
-            self.sound_cue = self.currentTrial[2]
-            self.left_visual_cue = self.currentTrial[0]
-            self.right_visual_cue = self.currentTrial[1]
-
             self.training_mode = self.currentTrial[3]
-
-            self.start_chamber = self._widget.startCellBtnGroup.checkedId()
-                
-        if self.sound_cue == "white_noise":
-            if self.left_visual_cue == "triangle":
-                self.success_chamber = self.left_chamber
-                self.error_chamber = self.right_chamber
-            else:
-                self.success_chamber = self.right_chamber
-                self.error_chamber = self.left_chamber
-        else:
-            if self.left_visual_cue == "square":
-                self.success_chamber = self.left_chamber
-                self.error_chamber = self.right_chamber
-            else:
-                self.success_chamber = self.right_chamber
-                self.error_chamber = self.left_chamber
-
-        if self.training_mode == "forced_choice":          
-            if self.success_chamber == self.left_chamber:
-                self.wallStates.wall = self.door_map_open
-            else:
-                self.wallStates.wall = self.right_goal_door_open
-                self.door_pub.publish(self.wallStates)
-        elif self.training_mode == "choice":
-            self.wallStates.wall = self.door_map_open
+           
 
     def display_excel_content(self, file_path):
         # Display the content in the QListWidget
@@ -506,8 +434,6 @@ class Interface(Plugin):
                 self.project_left_cue(self.left_visual_cue)
                 self.project_right_cue(self.right_visual_cue)
 
-                self.training_mode = self.currentTrial[3]
-
                 self.start_chamber = self._widget.startCellBtnGroup.checkedId()
                 
                 if self.sound_cue == "white_noise":
@@ -533,7 +459,7 @@ class Interface(Plugin):
 
         elif self.mode == Mode.RAT_IN_START_CHAMBER:
             if (self.current_time - self.mode_start_time).to_sec() >= self.start_wait_duration.to_sec():
-                self.wallStates.wall = self.start_door_open      #open start doors
+                self.wallStates.wall = self.start_door_open                                                #open start doors
                 self.door_pub.publish(self.wallStates)
                 self.mode_start_time = rospy.Time.now()
                 self.mode = Mode.START_TO_CHOICE
@@ -546,18 +472,18 @@ class Interface(Plugin):
 
         elif self.mode == Mode.CHOICE:
             rospy.loginfo("CHOICE")
-            self.wallStates.wall = self.start_door_close    #close start doors
+            self.wallStates.wall = self.walls_list                                                   #close start doors
             self.door_pub.publish(self.wallStates)
 
             if (self.current_time - self.mode_start_time).to_sec() >= self.choice_wait_duration.to_sec():
-                if self.training_mode == "forced_choice":          #open choice doors
+                if self.training_mode in ["forced_choice", "user_defined_forced_choice"]:                    #open choice doors
                     if self.success_chamber == self.left_chamber:
                         self.wallStates.wall = self.left_goal_door_open
                         self.door_pub.publish(self.wallStates)
                     else:
                         self.wallStates.wall = self.right_goal_door_open
                         self.door_pub.publish(self.wallStates)
-                elif self.training_mode == "choice":
+                elif self.training_mode ["choice", "user_defined_choice"]:
                     self.wallStates.wall = self.both_doors_open
                     self.door_pub.publish(self.wallStates)
     
@@ -576,14 +502,16 @@ class Interface(Plugin):
 
         elif self.mode == Mode.SUCCESS:
             rospy.loginfo("SUCCESS")
-            self.door_deactivate()
+            self.wallStates.wall = self.walls_list                                     #close choice doors
+            self.door_pub.publish(self.wallStates)
             self.reward_dispense()
             if (self.current_time - self.start_time).to_sec() == self.reward_duration.to_sec():
                 self.mode_start_time = rospy.Time.now()
                 self.mode = Mode.END_TRIAL
                 
         elif self.mode == Mode.ERROR:
-            self.door_deactivate()
+            self.wallStates.wall = self.walls_list                                       #close choice doors
+            self.door_pub.publish(self.wallStates)
             if (self.current_time - self.start_time).to_sec() == self.wrong_choice_duration.to_sec():
                 self.mode_start_time = rospy.Time.now()
                 self.mode = Mode.END_TRIAL
