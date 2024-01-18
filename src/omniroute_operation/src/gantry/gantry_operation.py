5f66504912ae5273b903afaf7cede6e8ca69e2a2
afsoongh <afsoon.gharib@gmail.com>
2024-01-18 12:49:30 -0800

    Working on gantry opperations.

--- src/omniroute_operation/src/gantry/gantry_operation.py
+++ src/omniroute_operation/src/gantry/gantry_operation.py
@@ -13,27 +13,18 @@ from gantry.gcodeclient import Client as GcodeClient
 class GantryFeeder:
     # @brief Initialize the GantryFeeder class
     def __init__(self):
-
-        # @brief Initialize the publisher for writing to '/Esmacat_write_maze_ard0_ease' topic
-        # self.maze_ard0_pub = rospy.Publisher('/Esmacat_write_maze_ard0_ease', ease_registers, queue_size=1)
-
-        # @brief Initialize the subsrciber for reading from '/csv_file_name' topic
-        rospy.Subscriber('/gantry_cmd', GantryCmd, self.gantry_cmd_callback, queue_size=1, tcp_nodelay=True)
-        rospy.Subscriber('/natnet_ros/Gantry/pose', PoseStamped, self.gantry_pose_callback, queue_size=1, tcp_nodelay=True)
-        rospy.Subscriber('/natnet_ros/Harness/pose', PoseStamped, self.harness_pose_callback, queue_size=1, tcp_nodelay=True)
-
-        rospy.Subscriber('/natnet_ros/MazeBoundary/marker0/pose', PointStamped, self.mazeboundary_marker0_callback, queue_size=1, tcp_nodelay=True)
-        rospy.Subscriber('/natnet_ros/MazeBoundary/marker1/pose', PointStamped, self.mazeboundary_marker1_callback, queue_size=1, tcp_nodelay=True)
-        rospy.Subscriber('/natnet_ros/MazeBoundary/marker2/pose', PointStamped, self.mazeboundary_marker2_callback, queue_size=1, tcp_nodelay=True)
-        rospy.Subscriber('/natnet_ros/MazeBoundary/marker3/pose', PointStamped, self.mazeboundary_marker3_callback, queue_size=1, tcp_nodelay=True)
-
         self.gantry_pose = PoseStamped()
         self.harness_pose = PoseStamped()
 
-        self.mazeboundary_marker0 = np.zeros(3)
-        self.mazeboundary_marker1 = np.zeros(3)
-        self.mazeboundary_marker2 = np.zeros(3)
-        self.mazeboundary_marker3 = np.zeros(3)
+        # @brief Initialize the subsrciber for reading from '/csv_file_name' topic
+        rospy.Subscriber('/gantry_cmd', GantryCmd, self.gantry_cmd_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/harness_pose_in_maze', PoseStamped, self.harness_pose_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/gantry_pose_in_maze', PoseStamped, self.gantry_pose_callback, queue_size=1, tcp_nodelay=True)
+
+        self.natnet_gantry_pose = PoseStamped()
+        self.natnet_harness_pose = PoseStamped()
+        rospy.Subscriber('/natnet_ros/Harness/pose', PoseStamped, self.natnet_harness_pose_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/natnet_ros/Gantry/pose', PoseStamped, self.natnet_gantry_pose_callback, queue_size=1, tcp_nodelay=True)
 
         self.track_harness = True
 
@@ -43,7 +34,7 @@ class GantryFeeder:
 
         # Wait for a few secs
         # time.sleep(1)
-        self.home()
+        # self.home()
 
         time.sleep(1)
 
@@ -53,48 +44,33 @@ class GantryFeeder:
             r.sleep()
         
     def loop(self):
-        # Define gantry xhat and yhat according to the maze boundary markers
-        self.xhat = self.mazeboundary_marker0 - self.mazeboundary_marker3
-        self.yhat = self.mazeboundary_marker1 - self.mazeboundary_marker0
-
-        # print("Length of xhat: ", np.linalg.norm(self.xhat))
-        # print("Length of yhat: ", np.linalg.norm(self.yhat))
-
-        self.xhat = self.xhat / np.linalg.norm(self.xhat)
-        self.yhat = self.yhat / np.linalg.norm(self.yhat)
-        
         if self.track_harness:
             # Vector from gantry to harness
             self.gantry_to_harness = np.array([self.harness_pose.pose.position.x - self.gantry_pose.pose.position.x, 
                                                self.harness_pose.pose.position.y - self.gantry_pose.pose.position.y, 
                                                self.harness_pose.pose.position.z - self.gantry_pose.pose.position.z])
             
+            self.natnet_gantry_to_harness = np.array([self.natnet_harness_pose.pose.position.x - self.natnet_gantry_pose.pose.position.x, 
+                                                      self.natnet_harness_pose.pose.position.y - self.natnet_gantry_pose.pose.position.y, 
+                                                      self.natnet_harness_pose.pose.position.z - self.natnet_gantry_pose.pose.position.z])
+            
             gh_dist = np.linalg.norm(self.gantry_to_harness)
 
             k = 10.0
 
-            # X component of the harness movement vector
-            x = k*np.dot(self.gantry_to_harness, self.xhat)
-            # Y component of the harness movement vector
-            y = k*np.dot(self.gantry_to_harness, self.yhat)
+            rospy.loginfo("Gantry to Harness Distance: {}".format(gh_dist))
+            rospy.loginfo("Gantry to Harness Vector: {}".format(self.gantry_to_harness))
 
-            # print("X: ", x, "Y: ", y)
-            # Move the gantry to the specified location
-            # TODO: Check if the gantry is within the maze boundary
-            if ~np.isnan(x) and ~np.isnan(y):
-                self.move_gantry_rel(x, y)
-    
-    def mazeboundary_marker0_callback(self, msg):
-        self.mazeboundary_marker0 = np.array([msg.point.x, msg.point.y, msg.point.z])
+            # # X component of the harness movement vector
+            # x = k*np.dot(self.gantry_to_harness, self.xhat)
+            # # Y component of the harness movement vector
+            # y = k*np.dot(self.gantry_to_harness, self.yhat)
 
-    def mazeboundary_marker1_callback(self, msg):
-        self.mazeboundary_marker1 = np.array([msg.point.x, msg.point.y, msg.point.z])
-    
-    def mazeboundary_marker2_callback(self, msg):
-        self.mazeboundary_marker2 = np.array([msg.point.x, msg.point.y, msg.point.z])
-    
-    def mazeboundary_marker3_callback(self, msg):
-        self.mazeboundary_marker3 = np.array([msg.point.x, msg.point.y, msg.point.z])
+            # # print("X: ", x, "Y: ", y)
+            # # Move the gantry to the specified location
+            # # TODO: Check if the gantry is within the maze boundary
+            # if ~np.isnan(x) and ~np.isnan(y):
+            #     self.move_gantry_rel(x, y)
     
     def home(self):
         self.gcode_client.raw_command("$25=5000")
@@ -126,12 +102,19 @@ class GantryFeeder:
             self.track_harness = False
             # Move the gantry to the specified location
             self.move_gantry_abs(msg.args[0], msg.args[1])
-        elif msg.cmd == "FEED":
+        elif msg.cmd == "PUMP":
             self.track_harness = False
             self.run_pump(msg.args[0])
         elif msg.cmd == "TRACK_HARNESS":
             self.track_harness = True
 
+    def natnet_gantry_pose_callback(self, msg):
+        self.natnet_gantry_pose = msg
+        # print("Natnet Gantry Pose: ", msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+    
+    def natnet_harness_pose_callback(self, msg):
+        self.natnet_harness_pose = msg
+        # print("Natnet Harness Pose: ", msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
 
     def gantry_pose_callback(self, msg):
         self.gantry_pose = msg
