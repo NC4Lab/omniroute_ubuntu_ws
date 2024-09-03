386448af80ce252a60e62338ef3272b6ae05d6b0
afsoongh <afsoon.gharib@gmail.com>
2024-09-03 15:04:15 -0700

    Copying over lost gantry stuff from old branch.

--- arduino/arduino_sketches/MazeGantryOperation/src/main.cpp
+++ arduino/arduino_sketches/MazeGantryOperation/src/main.cpp
@@ -1,12 +1,12 @@
-// ######################################
+// #################################
 
-//====== FeederOperation.ino ======
+// =========== main.ino ============
 
-// ######################################
+// #################################
 
 /**
- * @file Main Arduino INO file for running the maze.
- */
+ @file Arduino INO file for running MazeGantryOpperation.
+*/
 
 // BUILT IN
 #include "Arduino.h"
@@ -14,8 +14,8 @@
 
 // LOCAL
 #include <MazeDebug.h>
-#include <FeederServo.h>
 #include <EsmacatCom.h>
+#include <GantryOperation.h>
 
 //============ VARIABLES ===============
 
@@ -24,26 +24,42 @@ bool DB_VERBOSE = 1;  // set to control debugging behavior [0:silent, 1:verbose]
 bool DO_ECAT_SPI = 1; // set to control block SPI [0:dont start, 1:start]
 
 // Initialize class instances for local libraries
-MazeDebug Dbg;
-FeederServo FdSrv;
-EsmacatCom EsmaCom(10);
+MazeDebug Dbg;         // instance of MazeDebug class for debugging messages
+GantryOperation GanOp; // instance of GantryOperation class
 
 //=============== SETUP =================
 void setup()
 {
   // Setup serial coms
   Serial.begin(115200);
+  Serial1.begin(115200);
   delay(100);
 
   // Print setup started
   Dbg.printMsg(Dbg.MT::HEAD1, "RUNNING SETUP");
 
   // Initialize servos
-  FdSrv.initServo();
+  GanOp.servoInit();
   delay(1000);
 
-  // Initialize ethercat coms
-  EsmaCom.initEcat(true);
+  //   // TEMP
+  // GanOp.grblWrite("F30000", false);
+  // GanOp.grblWrite("$110", false);
+  // while (Serial1.available() > 0)
+  // {
+  //   Serial.write(Serial1.read());
+  // }
+
+  // // TEMP
+  // GanOp.grblInit();
+  // GanOp.gantryHome();
+  // //GanOp.gantryMove(500.0, 500.0);
+  // for (int i = 0; i < 50; i++)
+  // {
+  //   GanOp.gantryMove(5.0, 5.0);
+  //   //delay(10);
+  //   //Serial1.println("$J=G91 G21 X5.00 Y5.00 F25000"); 
+  // }
 
   // Print setup complete
   Dbg.printMsg(Dbg.MT::HEAD1, "SETUP COMPLETE");
@@ -52,42 +68,14 @@ void setup()
 //=============== LOOP ==================
 void loop()
 {
+  // TEMP
+  Dbg.dtTrack(1);
 
   // Check ethercat coms
-  if (!EsmaCom.readEcatMessage())
+  if (!GanOp.EsmaCom.readEcatMessage())
     return;
 
-  // Lower feeder
-  if (EsmaCom.rcvEM.msgTp == EsmaCom.MessageType::LOWER_FEEDER)
-  {
-    FdSrv.lowerFeeder();
-  }
-
-  // Raise feeder
-  if (EsmaCom.rcvEM.msgTp == EsmaCom.MessageType::RAISE_FEEDER)
-  {
-    FdSrv.raiseFeeder();
-  }
-
-  // Start pump
-  if (EsmaCom.rcvEM.msgTp == EsmaCom.MessageType::START_PUMP)
-  {
-    FdSrv.startPump();
-  }
-
-  // Stop pump
-  if (EsmaCom.rcvEM.msgTp == EsmaCom.MessageType::STOP_PUMP)
-  {
-    FdSrv.stopPump();
-  }
-
-  // Run full feeder opperation
-  if (EsmaCom.rcvEM.msgTp == EsmaCom.MessageType::REWARD)
-  {
-    FdSrv.runFeeder(2000);
-  }
-
-  // Send back recieved message arguments
-  EsmaCom.writeEcatAck(EsmaCom.ErrorType::ERR_NONE, EsmaCom.rcvEM.ArgU.ui8, EsmaCom.rcvEM.argLen);
-
+  // Process and execute ethercat arguments
+  GanOp.procEcatMessage();
+  Dbg.printMsg(Dbg.MT::INFO, "[loop] dt[%s]", Dbg.dtTrack());
 }
