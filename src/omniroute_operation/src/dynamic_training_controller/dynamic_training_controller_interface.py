03e5595b6d3194daf871520ffcdca583d445a90c
Afsoongh <afsoon.gharib@gmail.com>
2024-10-24 12:22:11 -0700

    minor edit

--- src/omniroute_operation/src/dynamic_training_controller/dynamic_training_controller_interface.py
+++ src/omniroute_operation/src/dynamic_training_controller/dynamic_training_controller_interface.py
@@ -57,143 +57,141 @@ class Interface(Plugin):
     def __init__(self, context):
         super(Interface, self).__init__(context)
 
-        if context is not None:
+        self._joint_sub = None
+        self.setObjectName('Dynamic Training Controller Interface')
 
-            self._joint_sub = None
-            self.setObjectName('Dynamic Training Controller Interface')
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        parser.add_argument("-q", "--quiet", action="store_true",
+                            dest="quiet",
+                            help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+        if not args.quiet:
+            print('arguments: ', args)
+            print('unknowns: ', unknowns)
 
-            from argparse import ArgumentParser
-            parser = ArgumentParser()
-            parser.add_argument("-q", "--quiet", action="store_true",
-                                dest="quiet",
-                                help="Put plugin in silent mode")
-            args, unknowns = parser.parse_known_args(context.argv())
-            if not args.quiet:
-                print('arguments: ', args)
-                print('unknowns: ', unknowns)
+        # Create QWidget
+        self._widget = QWidget()
+        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dynamic_training_controller_interface.ui')
+        # Extend the widget with all attributes and children from UI file
+        loadUi(ui_file, self._widget)
 
-            # Create QWidget
-            self._widget = QWidget()
-            ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dynamic_training_controller_interface.ui')
-            # Extend the widget with all attributes and children from UI file
-            loadUi(ui_file, self._widget)
+        rospy.loginfo('Test Interface started')
 
-            rospy.loginfo('Test Interface started')
+        self._widget.setObjectName('InterfacePluginUi')
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(
+                self._widget.windowTitle() + (' (%d)' % context.serial_number()))
 
-            self._widget.setObjectName('InterfacePluginUi')
-            if context.serial_number() > 1:
-                self._widget.setWindowTitle(
-                    self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+        # Add widget to the user interface
+        context.add_widget(self._widget)
+        
+        self.scene = QGraphicsScene()
+        
 
-            # Add widget to the user interface
-            context.add_widget(self._widget)
-          
-            self.scene = QGraphicsScene()
-            
+        # Define all buttons in the interface
+        self._widget.dynamicTrainingBtn.clicked.connect(self._handle_dynamicTrainingBtn_clicked)
+        self._widget.pseudorandomTrainingBtn.clicked.connect(self._handle_pseudorandomTrainingBtn_clicked) # Depending on how we design the dynamic controller, we may need more than one control training mode
+        self._widget.testingPhaseBtn.clicked.connect(self._handle_testingPhaseBtn_clicked)
+        self._widget.contTMazeBtn.clicked.connect(self._handle_contTMazeBtn_clicked)
+        self._widget.lowerAllDoorsBtn.clicked.connect(self._handle_lowerAllDoorsBtn_clicked)
+        # The starting chamber is always the same so no need to define it in the interface
 
-            # Define all buttons in the interface
-            self._widget.dynamicTrainingBtn.clicked.connect(self._handle_dynamicTrainingBtn_clicked)
-            self._widget.pseudorandomTrainingBtn.clicked.connect(self._handle_pseudorandomTrainingBtn_clicked) # Depending on how we design the dynamic controller, we may need more than one control training mode
-            self._widget.testingPhaseBtn.clicked.connect(self._handle_testingPhaseBtn_clicked)
-            self._widget.contTMazeBtn.clicked.connect(self._handle_contTMazeBtn_clicked)
-            self._widget.lowerAllDoorsBtn.clicked.connect(self._handle_lowerAllDoorsBtn_clicked)
-            # The starting chamber is always the same so no need to define it in the interface
+        self.is_testing_phase = False
+        self.trial_generator = False
 
-            self.is_testing_phase = False
-            self.trial_generator = False
+        # Define all Publishers
+        self.gantry_pub = rospy.Publisher('/gantry_cmd', GantryCmd, queue_size=1)
+        self.write_sync_ease_pub = rospy.Publisher('/Esmacat_write_sync_ease', ease_registers, queue_size=1)
+        self.event_pub = rospy.Publisher('/event', Event, queue_size=1)
+        self.button_pub = rospy.Publisher('/button', String, queue_size=1)
+        self.experiment_pub = rospy.Publisher('/experiment', String, queue_size=1)
+        self.sound_pub = rospy.Publisher('/sound', String, queue_size=1)
 
-            # Define all Publishers
-            self.gantry_pub = rospy.Publisher('/gantry_cmd', GantryCmd, queue_size=1)
-            self.write_sync_ease_pub = rospy.Publisher('/Esmacat_write_sync_ease', ease_registers, queue_size=1)
-            self.event_pub = rospy.Publisher('/event', Event, queue_size=1)
-            self.button_pub = rospy.Publisher('/button', String, queue_size=1)
-            self.experiment_pub = rospy.Publisher('/experiment', String, queue_size=1)
-            self.sound_pub = rospy.Publisher('/sound', String, queue_size=1)
+        # Define all Subscribers
+        rospy.Subscriber('/mode', String, self.mode_callback, queue_size=1)
+        rospy.Subscriber('/rat_head_chamber', Int8, self.rat_head_chamber_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/rat_body_chamber', Int8, self.rat_body_chamber_callback, queue_size=1, tcp_nodelay=True)
 
-            # Define all Subscribers
-            rospy.Subscriber('/mode', String, self.mode_callback, queue_size=1)
-            rospy.Subscriber('/rat_head_chamber', Int8, self.rat_head_chamber_callback, queue_size=1, tcp_nodelay=True)
-            rospy.Subscriber('/rat_body_chamber', Int8, self.rat_body_chamber_callback, queue_size=1, tcp_nodelay=True)
+        self.rat_head_chamber = -1
+        self.rat_body_chamber = -1
 
-            self.rat_head_chamber = -1
-            self.rat_body_chamber = -1
+        # Time for setting up publishers and subscribers
+        rospy.sleep(1.0)
 
-            # Time for setting up publishers and subscribers
-            rospy.sleep(1.0)
+        self.experiment_pub.publish("dynamic_training_controller_experiment")
 
-            self.experiment_pub.publish("dynamic_training_controller_experiment")
+        ## EXPERIMENTAL PARAMETERS
 
-            ## EXPERIMENTAL PARAMETERS
+        # File & Training parameters
+        self.current_file_index = 0
+        self.training_mode = None
 
-            # File & Training parameters
-            self.current_file_index = 0
-            self.training_mode = None
+        # Trial parameters
+        self.currentTrial = []
+        self.currentTrialNumber = 0
+        self.nTrials = 0
+        self.trials = []
+        self.current_trial_index = 0
 
-            # Trial parameters
-            self.currentTrial = []
-            self.currentTrialNumber = 0
-            self.nTrials = 0
-            self.trials = []
-            self.current_trial_index = 0
+        # State parameters
+        self.mode = Mode.START
+        self.mode_start_time = rospy.Time.now()
+        self.current_time = rospy.Time.now()
 
-            # State parameters
-            self.mode = Mode.START
-            self.mode_start_time = rospy.Time.now()
-            self.current_time = rospy.Time.now()
+        # Time parameters
+        self.timer = QTimer(self)
+        #self.timer.timeout.connect(self.run_experiment)
+        self.timer.start(10)
 
-            # Time parameters
-            self.timer = QTimer(self)
-            #self.timer.timeout.connect(self.run_experiment)
-            self.timer.start(10)
+        # Delay parameters (change durations later)
+        self.inter_trial_interval = rospy.Duration(1.0) # time between trials
+        self.sound_delay = rospy.Duration(0.5) # Delay between sound cue and lowering the doors of the start chamber 
+        self.choice_delay = rospy.Duration(1.5) # Duration to wait for rat to move to the choice point
+        self.reward_start_delay = rospy.Duration(13) # Duration to wait to for the reward to dispense
+        self.reward_end_delay = rospy.Duration(2) # Duration to wait to dispense reward if the rat made the right choice
+        self.success_delay = rospy.Duration(1) # Delay after reward ends
+        self.error_delay = rospy.Duration(2) # Delay after error
 
-            # Delay parameters (change durations later)
-            self.inter_trial_interval = rospy.Duration(1.0) # time between trials
-            self.sound_delay = rospy.Duration(0.5) # Delay between sound cue and lowering the doors of the start chamber 
-            self.choice_delay = rospy.Duration(1.5) # Duration to wait for rat to move to the choice point
-            self.reward_start_delay = rospy.Duration(13) # Duration to wait to for the reward to dispense
-            self.reward_end_delay = rospy.Duration(2) # Duration to wait to dispense reward if the rat made the right choice
-            self.success_delay = rospy.Duration(1) # Delay after reward ends
-            self.error_delay = rospy.Duration(2) # Delay after error
+        # Stimulus parameters
+        self.play_left_sound_cue = 0
+        self.play_right_sound_cue = 0
 
-            # Stimulus parameters
-            self.play_left_sound_cue = 0
-            self.play_right_sound_cue = 0
+        # Chamber parameters
+        self.success_chamber = 0
+        self.error_chamber = 0
+        self.start_chamber = 0
+        self.central_chamber = 0
+        self.choice_chamber = 0
+        self.left_return_chamber = 0
+        self.right_return_chamber = 0
 
-            # Chamber parameters
-            self.success_chamber = 0
-            self.error_chamber = 0
-            self.start_chamber = 0
-            self.central_chamber = 0
-            self.choice_chamber = 0
-            self.left_return_chamber = 0
-            self.right_return_chamber = 0
+        # Wall parameters
+        self.start_wall = Wall(0, 0)
+        self.left_goal_entry_wall = Wall(0, 0)
+        self.right_goal_entry_wall = Wall(0,0)
+        self.left_goal_exit_wall = Wall(0,0)
+        self.right_goal_exit_wall = Wall(0,0)
+        self.left_return_wall = Wall(0,0)
+        self.right_return_wall = Wall(0,0)
 
-            # Wall parameters
-            self.start_wall = Wall(0, 0)
-            self.left_goal_entry_wall = Wall(0, 0)
-            self.right_goal_entry_wall = Wall(0,0)
-            self.left_goal_exit_wall = Wall(0,0)
-            self.right_goal_exit_wall = Wall(0,0)
-            self.left_return_wall = Wall(0,0)
-            self.right_return_wall = Wall(0,0)
+        # Rat parameters
+        self.rat_position = 0
+        current_rat_chamber = self.rat_head_chamber
 
-            # Rat parameters
-            self.rat_position = 0
-            current_rat_chamber = self.rat_head_chamber
+        # Maze parameters
+        self.maze_dim = MazeDimensions()
 
-            # Maze parameters
-            self.maze_dim = MazeDimensions()
+        # Common functions
+        self.common_functions = CommonFunctions()
 
-            # Common functions
-            self.common_functions = CommonFunctions()
+        # Trial Types [Sound cue]
+        self.trial_types = {
+            1: ['1KHz'],
+            2: ['8KHz']
+        }
 
-            # Trial Types [Sound cue]
-            self.trial_types = {
-                1: ['1KHz'],
-                2: ['8KHz']
-            }
-
-            self.trial_count = {key: 0 for key in self.trial_types} 
+        self.trial_count = {key: 0 for key in self.trial_types} 
 
     # Define actions for clicking each button in the interface
     def _handle_dynamicTrainingBtn_clicked(self):
