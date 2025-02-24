9477e4354ff551f4126571e114cfb34c292ed444
Afsoongh <afsoon.gharib@gmail.com>
2025-02-24 12:53:00 -0800

    Working on setting up the manuscript testing code on this branch.

--- src/omniroute_operation/setup.py
+++ src/omniroute_operation/setup.py
@@ -11,7 +11,7 @@ d = generate_distutils_setup(
     
     # Define the packages to be included in the distribution
     packages=['omniroute_controller', 'experiment_controller',
-              'gantry', 'rat_detector', 'transformer', 'ts_sync', 'shared_utils', 'single_T_maze', 'dynamic_training_controller', 'rule_based_experiment', 'pseudorandom_control'],
+              'gantry', 'rat_detector', 'transformer', 'ts_sync', 'shared_utils', 'single_T_maze', 'dynamic_training_controller', 'rule_based_experiment', 'pseudorandom_control', 'gate_manuscript_testing'],
     
     # Specify the root directory for the package source files
     package_dir={'': 'src'},
