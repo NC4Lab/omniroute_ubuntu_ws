b5dad8a984d315c76673993d94844733c5e23014
Afsoongh <afsoon.gharib@gmail.com>
2025-03-03 17:16:46 -0800

    Adding explicit command queueing to the gantry code, jog cancel finally working

--- arduino/libraries/GantryOperation/src/GantryOperation.h
+++ arduino/libraries/GantryOperation/src/GantryOperation.h
@@ -53,7 +53,7 @@ public:
 	const int pumpStopSpeed = 1500; // Servo run speed (Forward: 500us to 1400us, Backward: 1600us to 2500us, Stop: 1500us)
 
 	bool feederRunning = false; 	// Flag to track the feeder state. [0:stopped, 1:running] No movement of gantry while feeder is running.
-	String queuedCmd = "";			// Queued command to be executed after the feeder stops.
+	String queuedCmd = "";			// Queued movement command to be executed after the feeder stops.
 
 	// Instance of EsmacatCom class using SPI chip select pin 10
 	EsmacatCom EsmaCom;
