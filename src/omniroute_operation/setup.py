79b8550e1fb568ea64cf5363351eead6928e8cf6
Afsoongh <afsoon.gharib@gmail.com>
2025-07-27 17:07:10 -0700

    Minor.

--- src/omniroute_operation/setup.py
+++ src/omniroute_operation/setup.py
@@ -1,46 +1,39 @@
-#!/usr/bin/env python
-
 # Import necessary modules
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # Generate the setup configuration dictionary using the generate_distutils_setup function
 d = generate_distutils_setup(
-    ## Uncomment the line below to include globally visible scripts (not recommended unless necessary)
-    # scripts=['bin/myscript'], 
-    
     # Define the packages to be included in the distribution
-<<<<<<< HEAD
-    packages=['omniroute_controller', 
-              'experiment_controller',
-              'gantry', 'rat_detector', 
-              'transformer', 
-              'ts_sync', 
-              'shared_utils', 
-              'single_T_maze', 
-              'dynamic_training_controller', 
-              'rule_based_experiment', 
-              'pseudorandom_control', 
-              'gate_manuscript_testing'],
-=======
-    packages=['omniroute_controller', 'experiment_controller',
-              'gantry', 'rat_detector', 'transformer', 'ts_sync', 'shared_utils', 'single_T_maze', 'dynamic_training_controller', 'rule_based_experiment', 'pseudorandom_control', 'radial_maze'],
->>>>>>> origin/master
-    
+    packages=[
+        'omniroute_controller', 
+        'experiment_controller',
+        'gantry', 
+        'rat_detector', 
+        'transformer', 
+        'ts_sync', 
+        'shared_utils', 
+        'single_T_maze', 
+        'dynamic_training_controller', 
+        'rule_based_experiment', 
+        'pseudorandom_control', 
+        'gate_manuscript_testing',
+        'radial_maze'
+    ],
+
     # Specify the root directory for the package source files
     package_dir={'': 'src'},
-    
+
     # List of scripts to be installed and their locations
-<<<<<<< HEAD
-    scripts=['scripts/omniroute_controller', 
-             'scripts/experiment_controller', 
-             'scripts/single_T_maze', 
-             'scripts/dynamic_training_controller', 
-             'scripts/rule_based_experiment', 
-             'scripts/pseudorandom_control'],
-=======
-    scripts=['scripts/omniroute_controller', 'scripts/experiment_controller', 'scripts/single_T_maze', 'scripts/dynamic_training_controller', 'scripts/rule_based_experiment', 'scripts/pseudorandom_control', 'scripts/radial_maze'],
->>>>>>> origin/master
+    scripts=[
+        'scripts/omniroute_controller', 
+        'scripts/experiment_controller', 
+        'scripts/single_T_maze', 
+        'scripts/dynamic_training_controller', 
+        'scripts/rule_based_experiment', 
+        'scripts/pseudorandom_control',
+        'scripts/radial_maze'
+    ],
 )
 
 # Pass the configuration dictionary to the setup function to create the package
